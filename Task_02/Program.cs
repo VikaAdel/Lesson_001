857fe5f236af53f6893993dd34c3301aa5fb8b57
Viktorya <vika.kokorina.933@gmail.com>
2023-11-07 15:09:19 +0300

    Добавили 2 задачу

--- Task_02/Program.cs
+++ Task_02/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите сумму вклада: ");
+double sum = Convert.ToDouble(Console.ReadLine());
+
+if (sum < 100)
+{
+    sum += sum * 0.05;
+}
+else if (sum <= 200)
+{
+    sum += sum * 0.07;
+}
+else
+{
+    sum += sum * 0.1;
+}
+Console.WriteLine($"Сумма вклада после начисления процентов: {sum}");
+
+Console.ReadKey();
+
+
