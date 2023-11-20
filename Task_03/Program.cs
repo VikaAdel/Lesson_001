4559345cef3331b81f2da34136e7951b80ec8441
Viktorya <vika.kokorina.933@gmail.com>
2023-11-20 12:40:25 +0300

    Task_03

--- Task_03/Program.cs
+++ Task_03/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿///Напишите программу, которая принимает на вход целое число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+Console.Clear();
+Console.Write(" Введите двузначное число: ");
+int n = int.Parse(Console.ReadLine()!);
+while (n < 10 || n > 99)
+{ // 23 < 100 || 23 > 999 -> 1 + 0 = 1
+    Console.Write("Вы ошиблись!\nВведите двузначное число: ");
+    n = int.Parse(Console.ReadLine()!);
+}
+
+
+int LargeDigit = n % 10 > n / 10 ? n % 10 : n / 10;
+Console.WriteLine("наибольшая цифра числа: " + LargeDigit);
