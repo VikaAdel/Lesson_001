cb9b9d442e4309956781b58d3cb24dfacee64efd
Viktorya <vika.kokorina.933@gmail.com>
2023-11-27 13:27:18 +0300

    Task_04

--- Task_04/Program.cs
+++ Task_04/Program.cs
@@ -1,26 +1,35 @@
-﻿//Напишите программу, которая на вход принимает натуральное число N, а на выходе показывает его цифры через запятую.
+﻿using System;
 
-Console.Write("Введите натуральное число N: ");
-int N = Convert.ToInt32(Console.ReadLine());
-if (N < 10)
+class Program
 {
-    Console.WriteLine(N);
-}
-else
-{
-    while (N > 0)
+    static void Main()
     {
-        int currentDigit = N % 10;
-        N /= 10;
-        if (N > 0)
+        // Создаем массив из вещественных чисел с нулевой дробной частью
+        double[] numbers = { 2.2, 0.4, 9.11, 7.2, 78.98 };
+
+        // Находим минимальное и максимальное значение в массиве
+        double min = double.MaxValue;
+        double max = double.MinValue;
+
+        foreach (double number in numbers)
         {
-            Console.Write(currentDigit + ",");
+            if (number < min)
+            {
+                min = number;
+            }
+
+            if (number > max)
+            {
+                max = number;
+            }
         }
-        else
-        {
-            Console.WriteLine(currentDigit);
-        }
+
+        // Находим разницу между максимальным и минимальным элементом
+        double difference = max - min;
+
+        // Выводим результат
+        Console.WriteLine($"Минимальное значение: {min}");
+        Console.WriteLine($"Максимальное значение: {max}");
+        Console.WriteLine($"Разница между максимальным и минимальным элементом: {difference}");
     }
 }
-
-
