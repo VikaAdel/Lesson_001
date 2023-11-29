88759a386763aad5a14607d3a6cfa538e4c12229
Viktorya <vika.kokorina.933@gmail.com>
2023-11-29 15:27:11 +0300

    Seminar_4.2\

---
+++ Seminar_4.2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте массив заполненный случайными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве
+
+
+
+Console.Clear();
+// Создаем массив  из 10 целых чисел 
+
+int[] numbers = new int[10];
+Random random = new Random();
+
+int count = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = random.Next(100, 1000);
+    Console.Write(numbers[i] + " ");
+
+    if (numbers[i] % 2 == 0)
+    {
+        count++;
+    }
+}
+Console.Write($"количество чётных чисел в массиве: {count} ");
