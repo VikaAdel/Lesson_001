01c8e715b1eceff78cfad975b0fb8681d5115803
Viktorya <vika.kokorina.933@gmail.com>
2023-11-29 15:27:47 +0300

    Task_03

--- Task_03/Program.cs
+++ Task_03/Program.cs
@@ -2,30 +2,29 @@
 //Найдите разницу между максимальным и минимальным элементов массива.
 
 
-        // Создаем массив из вещественных чисел с нулевой дробной частью
-        double[] numbers = { 2.2, 0.4, 9.11, 7.2, 78.98 };
+// Создаем массив из вещественных чисел с нулевой дробной частью
+double[] numbers = { 2.2, 0.4, 9.11, 7.2, 78.98 };
 
-        // Находим минимальное и максимальное значение в массиве
-        double min = double.MaxValue;
-        double max = double.MinValue;
+// Находим минимальное и максимальное значение в массиве
+double min = double.MaxValue;
+double max = double.MinValue;
 
-        foreach (double number in numbers)
-        {
-            if (number < min)
-            {
-                min = number;
-            }
+foreach (double number in numbers)
+{
+    if (number < min)
+    {
+        min = number;
+    }
 
-            if (number > max)
-            {
-                max = number;
-            }
-        }
+    if (number > max)
+    {
+        max = number;
+    }
+}
 
-        // Находим разницу между максимальным и минимальным элементом
-        double difference = max - min;
+// Находим разницу между максимальным и минимальным элементом
+double difference = max - min;
 
-        // Выводим результат
-   
-        Console.WriteLine($"Разница между максимальным и минимальным элементом: {difference}");
- 
+// Выводим результат
+
+Console.WriteLine($"Разница между максимальным и минимальным элементом: {difference}");
