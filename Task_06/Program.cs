2056fb0bd119a7b3e5bd1bb4f453e6c8d16e325c
Viktorya <vika.kokorina.933@gmail.com>
2023-12-05 16:06:00 +0300

    Task_06

--- Task_06/Program.cs
+++ Task_06/Program.cs
@@ -102,12 +102,96 @@
 //     }
 // }
 
-void test(ref int m)
-{
-    m = m + 10;
-}
-Console.Clear();
-int n = 7;
-test(ref n);
-Console.Write(n);
 
+
+
+
+
+
+// Семинар 6. Массивы и строки
+
+// // Задание 1. Задание 1. Совместная работа. Задайте массив символов (тип char []). Создайте строку из символов этого массива.
+// Конструктор строки вида string(char []) не использовать.
+
+// Console.Clear();
+// // char[] chars = {'a', 'b', 'c', 'd'};
+// Console.Write("Введите символы через пробел: ");
+// char[] chars = Console.ReadLine()!.Split().Select(x => char.Parse(x)).ToArray();
+// /*
+// 1. "a b 1 2 3 4 6 c d e"
+// 2. ["a", "b", "1", "2", "3", "4", "6", "c", "d", "e"]
+// 3. 'a', 'b', '1', '2', '3', '4', '6', 'c', 'd', 'e'
+// 4. ['a', 'b', '1', '2', '3', '4', '6', 'c', 'd', 'e']
+// */
+// string result = string.Empty; // создание пустой строки
+// // string result = "";
+// foreach (char element in chars){
+//     result = result + element;
+// }
+// Console.WriteLine(result);
+
+
+
+
+
+
+
+// Задача 2:
+// На основе символов строки (тип string) сформировать массив символов (тип char[]). Вывести массив на экран.
+// Пример:
+// “Hello!” => [‘H’, ‘e’, ‘l’, ‘l’, ‘o’, ‘!’] 
+
+// Console.Clear();
+// Console.Write("Введите строку: ");
+// string initial = Console.ReadLine()!;
+// char[] chars = new char[initial.Length];
+// // Hello
+// // 01234
+// for (int i = 0; i < initial.Length; i++)
+// {
+//     chars[i] = initial[i];
+// }
+// Console.WriteLine($"Результат: [{string.Join(" ", chars)}]");
+
+
+
+
+
+
+
+
+// Задание 3. 
+// Семинар 6. Массивы и строки
+// Считать строку с консоли, состоящую из латинских букв в нижнем регистре. Выяснить, сколько среди введённых букв гласных.
+// Пример:
+// “hello” => 2
+// “world” => 1
+
+
+// bool is_vowels(char el, char[] chars)
+// {
+//     foreach (char x in chars)
+//     {
+//         if (x == el)
+//             return true;
+//     }
+//     return false;
+// }
+
+
+// Console.Clear();
+// Console.Write("Введите строку: ");
+// string initial = Console.ReadLine()!;
+// char[] chars = { 'a', 'e', 'u', 'i', 'o', 'y' };
+// int count = 0;
+// for (int i = 0; i < initial.Length; i++)
+// {
+//     if (is_vowels(initial[i], chars))
+//         count++;
+// }
+// Console.WriteLine(count);
+
+
+
+  
+    
