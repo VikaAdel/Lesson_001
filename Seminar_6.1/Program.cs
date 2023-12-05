e0539a076e86954ce341074f97ff21fb0205c3f9
Viktorya <vika.kokorina.933@gmail.com>
2023-12-05 16:04:31 +0300

    Seminar_6.1\

---
+++ Seminar_6.1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 1: Задайте двумерный массив символов (тип char [,]). Создать строку из символов этого массива.
+
+
+Console.Clear();
+char[,] charArray =
+{
+    {'H', 'e', 'l', 'l', 'o'},
+    {'W', 'o', 'r', 'l', 'd'}
+};
+//char[] chars = Console.ReadLine()!.Split().Select(x => char.Parse(x)).ToArray();
+
+string result = string.Empty; // создание пустой строки
+
+foreach (char element in charArray)
+{
+    result = result + element;
+}
+Console.WriteLine(result);
+//string initial = Console.ReadLine()!;
