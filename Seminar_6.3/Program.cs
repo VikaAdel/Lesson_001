7ab18709b0d3e385211bc263eef6331f2d5ed179
Viktorya <vika.kokorina.933@gmail.com>
2023-12-05 16:04:57 +0300

    Seminar_6.3\

---
+++ Seminar_6.3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 3: Задайте произвольную строку. Выясните, является ли она палиндромом.
+
+Console.Clear();
+
+Console.Write("Введите слово: ");
+string str = Console.ReadLine(); // Вводим строку
+str = str.Replace(" ", "").ToLower();
+string reverseStr = ""; // здесь будет наша перевернутая строка
+for (int i = str.Length - 1; i >= 0; i--)
+{
+    reverseStr += str[i]; // переворачиваем строку
+}
+if (str == reverseStr)
+    Console.WriteLine("Введенная строка является палиндромом.");
+else
+    Console.WriteLine("Введенная строка не является палиндромом.");
+
