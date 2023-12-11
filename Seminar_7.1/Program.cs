739746242680223dca953daebdc59d3a8a85d799
Viktorya <vika.kokorina.933@gmail.com>
2023-12-11 09:26:24 +0300

    Seminar_7.1\

---
+++ Seminar_7.1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа
+// в промежутке от M до N. Использовать рекурсию, не использовать циклы.
+
+
+//Взято с буткемпа 
+using static System.Console;
+
+void printNumbers(int current, int end)
+{
+    if (current <= end)
+    {
+        Write($"{current}\t");
+        printNumbers(current + 1, end);
+    }
+}
+
+Clear();
+Write("Введите значение M: ");
+int m = int.Parse(ReadLine()!);
+
+Write("Введите значение N: ");
+int n = int.Parse(ReadLine()!);
+
+printNumbers(m, n);
